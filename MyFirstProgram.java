cadb649b3da3ddc6ee689cf540b2b17d153389ca
Stanislav Marulin <stanislavmaru@gmail.com>
2026-02-11 10:31:04 +0200

    Update MyFirstProgram.java

--- MyFirstProgram.java
+++ MyFirstProgram.java
@@ -1,6 +1,7 @@
 public class MyFirstProgram {
     public static void main(String[] args) {
-        System.out.println("I`m a student");
+        System.out.println("I`m a student. Group&");
     }
 }
+
 
